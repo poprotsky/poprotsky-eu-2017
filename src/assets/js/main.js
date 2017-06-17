9627fe1a1d547c3c593366e830d79149474b37e6
poprocki <eugeniusz@iguanastudio.pl>
2017-06-17 17:06:52 +0200

    main styles, about, services, testimonials, contact, main header and footer and other small things

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,123 +1,143 @@
 $(document).ready(function(){
-	
-	if($.fn.fancybox) {
-		$.extend(true, $.fancybox.defaults, {
-			hash : false
-		});
+    //
+    // parallax
+    //
+    if(!device.mobile() && !device.tablet() && !$('html').hasClass('ie')) {
+      if($('[data-stellar-background-ratio]').length) {
+        $.stellar();
+      }
+    }
+    // typed
+    if(!device.mobile()) {
+        var $text = $('.typed').text();
+        $('.typed').typed({
+            strings: [$text],
+            showCursor: false,
+            typeSpeed: 5
+        });
+    }
+    //
+    // scroll down
+    //
+    $('.scroll-down').on('click', function(){
+      var $scroll_el = $(this).attr('href');
+      if ($($scroll_el).length) {
+        $('html, body').animate({
+          scrollTop: $($scroll_el).offset().top},
+          500);
+      }
+      return false;
+    });
+    //
+    // MAIN NAV
+    //
+    $('body')
+      .on('click', '.menu-trigger', function(){
+        $('body').toggleClass('nav-is-open');
+      })
+      .on('click touchend', function(e){
+        if(!$(e.target).closest('.menu-trigger').length) {
+          if(!$(e.target).closest('.main-header__nav').length) {
+            $('body').removeClass('nav-is-open');
+          }
+        }
+      })
+      .on('touchmove', function(e){
+        if($(this).hasClass('nav-is-open')) {
+          //if(!$(e.target).closest('.main-header__nav').length) {
+            e.preventDefault();
+          //}
+        }
+      });
 
-		$('.magnific').fancybox();
-	}
-	
-	if($.fn.validate) {
-		$.validator.setDefaults({
-			highlight: function(element, errorClass, validClass) {
-				var $element = $(element);
-				$element.addClass(errorClass).removeClass(validClass);
-				if ($element.attr('type') == 'checkbox' || $element.attr('type') == 'radio') {
-					if ($element.attr('type') == 'checkbox') {
-						$element.closest('.checkbox').addClass('error-checkbox');
-					} else if ($element.attr('type') == 'radio') {
-						$element.closest('.radio').addClass('error-radio');
-					}
-				} else if ($element.prop("tagName") == 'SELECT') {
-					$element.closest('.custom-select, .custom-select2, .custom-select2-multiple').addClass('select-error');
-				}
-			},
-			unhighlight: function(element, errorClass, validClass) {
-				var $element = $(element);
-				$element.removeClass(errorClass).addClass(validClass);
-				if ($element.attr('type') == 'checkbox' || $element.attr('type') == 'radio') {
-					if ($element.attr('type') == 'checkbox') {
-						$element.closest('.checkbox').removeClass('error-checkbox');
-					} else if ($element.attr('type') == 'radio') {
-						$element.closest('.radio').removeClass('error-radio');
-					}
-				} else if ($element.prop("tagName") == 'SELECT') {
-					$element.closest('.custom-select, .custom-select2, .custom-select2-multiple').removeClass('select-error');
-				}
-			},
-			errorPlacement: function() {
-			},
-			ignore: ':hidden:not([type="hidden"]):not(select)'
-		});
+    //
+    // scroll up
+    //
+    var $scroll_var,
+        $current_scroll_position,
+        $header_height = $('.main-header').outerHeight();
 
-		$('form.validate').each(function() {
-			$(this).validate();
-		});
-	}
-	
-    if($.fn.owlCarousel) {
-        $('.slider').owlCarousel({
-            items: 1,
-            margin: 0,
-            loop: true,
-            nav: true,
-            dots: true,
-            autoplay: true,
-            autoplayTimeout: 5000,
-            autoplayHoverPause: false,
-            stagePadding: 0,
-            navText: ['Poprzedni','Następny'],
-            responsiveClass: true,
-            responsive: {
-                0:{
-                    items: 1
-                },
-                600:{
-                    items: 1
-                },
-                1000:{
-                    items: 1
-                }
-            }
-        }); 
+    $(document).scroll(function(){
+      $current_scroll_position = $(document).scrollTop();
+      if($current_scroll_position > 0) {
+        // scroll Bottom
+        if ($scroll_var < $current_scroll_position) {
+          $('body').addClass('nav-is-fixed');
+          if($('body').hasClass('nav-is-visible')) {
+            $('body').addClass('nav-is-hidden');
+          }
+        }
+        // scroll Top
+        else if ($scroll_var > $current_scroll_position) {
+          $('body').addClass('nav-is-visible');
+          $('body').removeClass('nav-is-hidden');
+        }
+      }
+      // scrollTop == 0
+      else if($current_scroll_position <= 0){
+        $('body').removeClass('nav-is-fixed nav-is-visible nav-is-hidden');
+        $('.main-header').addClass('transition');
+        setTimeout(function(){
+          $('.main-header').removeClass('transition');
+        }, 300)
+      }
+      
+      $scroll_var = $current_scroll_position;
+    });
+
+
+    // 
+    // PLUGINS
+    // 
+    	
+    if($.fn.fancybox) {
+    	$.extend(true, $.fancybox.defaults, {
+    		hash : false
+    	});
+
+    	$('.magnific').fancybox();
+    }
+
+    if($.fn.validate) {
+    	$.validator.setDefaults({
+    		highlight: function(element, errorClass, validClass) {
+    			var $element = $(element);
+    			$element.addClass(errorClass).removeClass(validClass);
+    			if ($element.attr('type') == 'checkbox' || $element.attr('type') == 'radio') {
+    				if ($element.attr('type') == 'checkbox') {
+    					$element.closest('.checkbox').addClass('error-checkbox');
+    				} else if ($element.attr('type') == 'radio') {
+    					$element.closest('.radio').addClass('error-radio');
+    				}
+    			} else if ($element.prop("tagName") == 'SELECT') {
+    				$element.closest('.custom-select, .custom-select2, .custom-select2-multiple').addClass('select-error');
+    			}
+    		},
+    		unhighlight: function(element, errorClass, validClass) {
+    			var $element = $(element);
+    			$element.removeClass(errorClass).addClass(validClass);
+    			if ($element.attr('type') == 'checkbox' || $element.attr('type') == 'radio') {
+    				if ($element.attr('type') == 'checkbox') {
+    					$element.closest('.checkbox').removeClass('error-checkbox');
+    				} else if ($element.attr('type') == 'radio') {
+    					$element.closest('.radio').removeClass('error-radio');
+    				}
+    			} else if ($element.prop("tagName") == 'SELECT') {
+    				$element.closest('.custom-select, .custom-select2, .custom-select2-multiple').removeClass('select-error');
+    			}
+    		},
+    		errorPlacement: function() {
+    		},
+    		ignore: ':hidden:not([type="hidden"]):not(select)'
+    	});
+
+    	$('form.validate').each(function() {
+    		$(this).validate();
+    	});
     }
 	
-	if (typeof jcf == 'object') {
-		jcf.setOptions('Select', {
-			fakeDropInBody: false,
-		    maxVisibleItems: 5,
-		    wrapNative: false,
-		    wrapNativeOnMobile: true,
-			useCustomScroll: false
-		});
-
-		jcf.replace($('.custom-select select'));
-		jcf.replace($('[type="radio"]'));
-		jcf.replace($('[type="checkbox"]'));
-	}
-	
-	$('.wysiwyg iframe').each(function(){
-		$(this).wrap('<div class="embed-responsive embed-responsive--16by9"></div>');
-	});
-
-	$('.wysiwyg table').each(function(){
-		var mT = $(this).css('marginTop');
-		var mB = $(this).css('marginBottom');
-		$(this).css('marginBottom','0').css('marginTop','0').wrap('<div class="table-responsive" style="margin-bottom: '+mB+'; margin-top: '+mT+';"></div>');
-	});
-	
-	$('.menu-trigger').on('click', function(){
-		$('html').toggleClass('is-menu-open');
-	});
-    
-	minimizeTop(); 
 });
 
 $(window).on('load', function(){
 
 });
-
-function minimizeTop()
-{
-	var scrollOffset = parseInt($('.top').data('minimize-offset')) > 0 ? parseInt($('.top').data('minimize-offset')) : false;
-	if (scrollOffset !== false)
-	{
-		$(window).scroll(function(){
-			if ($(window).scrollTop() >= scrollOffset)
-				$('html').addClass('is-top-minimize');
-			else
-				$('html').removeClass('is-top-minimize');
-		}).scroll();
-	}
-}
